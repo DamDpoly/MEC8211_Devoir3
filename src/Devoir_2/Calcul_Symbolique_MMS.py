6cdfcee60f4acf7154e4f484f499e695ac6d1fc1
DamDpoly <damien.defrance@polymtl.ca>
2025-03-08 22:46:58 -0500

    Update Calcul_Symbolique_MMS.py

--- src/Devoir_2/Calcul_Symbolique_MMS.py
+++ src/Devoir_2/Calcul_Symbolique_MMS.py
@@ -8,7 +8,6 @@ Pour le devoir 2 de MEC8211
 
 """
 
-
 import numpy as np
 import matplotlib.pyplot as plt
 import sympy as sp
@@ -16,23 +15,23 @@ import sympy as sp
 # Définition des variables symboliques
 r, t, R, l, C_e, D_eff, k = sp.symbols('r t R l C_e D_eff k')
 
-# Manufactured solution
-C_MMS = ((r**2)/(R**2)) * (C_e + sp.exp(-l * t)) - sp.exp(-l * t)
+# solution MMS
+C_MMS = (((r**2)/(R**2)) * (C_e + sp.exp(-l * t)) - sp.exp(-l * t))
 
-# Calcul des dérivées
+# Appliquer l'opérateur sur la solution MMS
 C_t = sp.diff(C_MMS, t)
 C_r = sp.diff(C_MMS, r)
 C_rr = (1/r) * sp.diff(C_r * r, r)
 
-# Modifying source term to be a sink, proportional to concentration
-source = C_t - C_rr * D_eff + k * C_MMS    # Sink term proportional to concentration
+# Terme source
+source = C_t - C_rr * D_eff + k * C_MMS
 
 # Conditions aux limites et initiales
 C_initial = C_MMS.subs(t, 0)
 C_boundary_R = C_MMS.subs(r, R)
 dCdr_boundary_r0 = C_r.subs(r, 0)
 
-# Affichage des dérivées
+# Afficher les résultats
 print("Dérivée en temps :")
 print(C_t)
 print("\nDérivée première :")
@@ -48,25 +47,25 @@ print(C_boundary_R)
 print("\nCondition frontière Neumann dC/dx(t,0) :")
 print(dCdr_boundary_r0)
 
-# Conversion en fonctions Python
+# Créer une fonction appelable pour l'expression symbolique
 f_C_MMS_symbolic = sp.lambdify([r, t, R, l, C_e, D_eff, k], C_MMS, "numpy")
 f_source_symbolic = sp.lambdify([r, t, R, l, C_e, D_eff, k], source, "numpy")
 
-# Définition des paramètres
+# taille du domaine
 tmin, tmax = 0, 1
 rmin, rmax = 0, 1
 nt, nr = 50, 50
 
-# Création des maillages temporel et spatial
+# Établir une grille régulière de points d'interpolation
 tdom = np.linspace(tmin, tmax, nt)
 rdom = np.linspace(rmin, rmax, nr)
 ti, ri = np.meshgrid(tdom, rdom, indexing='ij')
 
-# Approximation numerique: compute the concentration and source term
+# Évaluer la fonction MMS et le terme source sur le maillage
 z_MMS = f_C_MMS_symbolic(ri, ti, 1, 1, 1, 1, 1)
 z_source = f_source_symbolic(ri, ti, 1, 1, 1, 1, 1)
 
-# Tracé des résultats
+# Tracer les résultats
 plt.figure()
 plt.contourf(ri, ti, z_MMS, levels=50)
 plt.colorbar()
@@ -82,3 +81,4 @@ plt.title('Terme Source')
 plt.xlabel('r')
 plt.ylabel('t')
 plt.show()
+
